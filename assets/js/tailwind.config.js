56f667fb37747ae0d9e9d15f839e6fdc2e1cc51e
xStephx <bojkovskistefan9@gmail.com>
2024-10-26 23:37:36 +0200

    initial setup

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                Tomato: 'hsl(4, 100%, 67%)',
+                DarkSlateGrey: 'hsl(234, 29%, 20%)',
+                CharcoalGrey: 'hsl(235, 18%, 26%)', 
+                Grey: 'hsl(231, 7%, 60%)', 
+                White: 'hsl(0, 0%, 100%)',
+            },
+        },
+        fontFamily: {
+            Roboto: ['Roboto', 'sans-serif'],
+        },
+    },
+    plugins: [],
+}
