d185a482bc542fb4229cab6729d8a902819d6b3f
xStephx <bojkovskistefan9@gmail.com>
2024-10-31 23:22:34 +0100

    added functionality on sign-up form, and success message

---
+++ assets/js/script.js
@@ -0,0 +1,44 @@
+const signUp = document.getElementById('sign-up')
+const errorMessage = document.getElementById('error-message')
+const emailInput = document.getElementById('email-input')
+const submitBtn = document.getElementById('submit-btn')
+const successMessage = document.getElementById('success-message')
+const successEmail = document.getElementById('success-email')
+const dismissMesage = document.getElementById('dismiss-message')
+
+function submit() {
+    const email = emailInput.value.trim()
+    const isValidEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+
+    if (!email) {
+        errorMessage.classList.remove('hidden')
+        emailInput.classList.add('text-Tomato', 'border-Tomato', 'bg-red-200')
+    } else if (!isValidEmail) {
+        errorMessage.classList.remove('hidden')
+        emailInput.classList.add('text-Tomato', 'border-Tomato', 'bg-red-200')
+    } else {
+        signUp.classList.add('hidden')
+        successMessage.classList.remove('hidden')
+        successEmail.textContent = email
+    }
+}
+
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+    submit()
+})
+
+emailInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault()
+        submit()
+    }
+})
+
+dismissMesage.addEventListener('click', () => {
+    signUp.classList.remove('hidden')
+    successMessage.classList.add('hidden')
+    emailInput.value = ''
+    errorMessage.classList.add('hidden')
+    emailInput.classList.remove('text-Tomato', 'border-Tomato', 'bg-red-200')
+})
